74fc1333ea5b01527d9d33aed85be741dcfbb786
baobeta <74138796+baobeta@users.noreply.github.com>
2021-04-02 11:24:45 +0700

    add heroku

--- Demo/todo_list/base/views.py
+++ Demo/todo_list/base/views.py
@@ -59,6 +59,7 @@ class TaskList(LoginRequiredMixin,ListView):
         context['tasks']=context['tasks'].filter(user=self.request.user)
         #Dem task chua thuc hien
         context['count'] = context['tasks'].filter(complete=False).count()
+        #search trong cac task trong DB
         search_input =self.request.GET.get('search-area') or ''
         if search_input:
             context['tasks']= context['tasks'].filter(title__icontains=search_input)
--- Demo/todo_list/todo_list/settings.py
+++ Demo/todo_list/todo_list/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +122,7 @@ LOGIN_URL='login'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
